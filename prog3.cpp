3d181b211ea2a9461152dca4ae52dbefd46bfb8d
Náthalie Lima <92655303+NathalieLima@users.noreply.github.com>
2023-08-25 08:51:40 -0300

    Create prog3.cpp

---
+++ prog3.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+int main(void) {
+    float number1;
+    float number2;
+    
+    cout << "A = ";
+    cin >> number1;
+    cout << "B = ";
+    cin >> number2;
+    
+    cout << "Soma: " << number1 + number2 << endl;
+    cout << "Subtração: " << number1 - number2 << endl;
+    cout << "Multiplicação: " << number1 * number2 << endl;
+    cout << "Divisão: " << number1 / number2 << endl;
+
+    return 0;
+}
