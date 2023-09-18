dec9e84e3ff9e187d50a0f203e007a09870a5608
NathalieLima <nathalielima3@gmail.com>
2023-09-18 12:32:08 -0300

    Organização de pastas e arquivos - 18/09

---
+++ PI - Programacao Imperativa/P007/exc9_fibonacci.cpp
@@ -0,0 +1,34 @@
+/*
+Escreva um programa em C++ que leia um número inteiro e imprima a sequência de 
+Fibonacci (Referência abaixo) até o número fornecido pelo usuário.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int num = 0, i = 0, soma = 0;
+    int a = 0, b = 1;
+    
+    
+    cout << "Entre com um número: ";
+    cin >> num;
+    
+    cout << "A sequência de Fibonacci até " << num << " é:";
+    
+    while (i < num)
+    {
+        cout << " " << a;   
+        
+        soma = a + b;
+        
+        a = b;
+        b = soma;
+        i++;
+    }
+    
+
+    return 0;
+}
