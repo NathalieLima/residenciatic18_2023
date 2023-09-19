69ac221ac3289819e8d6f856ccd4d1959904922f
NathalieLima <nathalielima3@gmail.com>
2023-09-19 11:15:28 -0300

    Aula/avaliação do dia 19 de set

---
+++ PI - Programacao Imperativa/A001/avaliacao1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Passageiro {
+    string cpf;
+    string nome;
+    int dt_nascimento;
+    int num_autorizacao;
+};
+
+struct Roteiro {
+    int codigo;
+    int data_hora_prevista;
+    int duracao_prevista;
+    string origem;
+    string destino;
+};
+
+int main (void) {
+
+
+
+    return 0;
+}
